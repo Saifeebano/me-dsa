d1eb0a0ad431e6a992f91b257ba4a23cfceac2c0
Saifee Bano <saifeebano2@gmail.com>
2026-09-02 00:50:23 +0530

    Time: 1 ms (99.98%) | Memory: 42.5 MB (63.76%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            
+            // Integer.MAX_VALUE is 2147483647 (ends in 7)
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            
+            // Integer.MIN_VALUE is -2147483648 (ends in -8)
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            rev = rev * 10 + pop;
+        }
+        
+        return rev;
+    }
+}
