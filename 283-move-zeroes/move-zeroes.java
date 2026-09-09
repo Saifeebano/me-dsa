eb82fcce9ad063105c2599ab0e7bbf2b8f2a1fe8
Saifee Bano <saifeebano2@gmail.com>
2026-09-10 00:08:30 +0530

    Time: 1 ms (99.89%) | Memory: 47.8 MB (57.26%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int insertPos = 0;
-       
-       for(int i=0; i<nums.length; i++){
-        if(nums[i] !=0){
+        int insertPos = 0;
 
-           int temp = nums[i];
-           nums[i] = nums[insertPos];
-           nums[insertPos] = temp;
-        
-        insertPos++;
-       }
+        // Step 1: Saare non-zero elements ko aage shift karo
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+
+        // Step 2: Baki bache hue positions par 0 fill karo
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
     }
-}
 }
